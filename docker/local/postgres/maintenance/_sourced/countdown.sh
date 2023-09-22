b669fdd731713ef364fc417b66a95e2fec6fe48d
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-09-21 22:49:19 -0700

    setup makefiels

--- docker/local/postgres/maintenance/_sourced/countdown.sh
+++ docker/local/postgres/maintenance/_sourced/countdown.sh
@@ -9,7 +9,7 @@ countdown(){
   local d=$(($(date +%s) + "${seconds}"))
 
   while [ "$d" -ge `date +%s` ]; do
-  
+
     echo -ne "$(date -u --date @$(($d - `date +%s`)) +%H:%M:%S)\r";
 
     sleep 0.1
