f7f3abaeb595317b68cce9af212d0e354120a3df
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-09-22 04:47:52 -0700

    setup celery

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -34,7 +34,7 @@ DJANGO_APPS = [
 ]
 
 THIRD_PARTY_APPS = ["rest_framework", "django_filters", "corsheaders",
-                    "django_countries", "phonenumber_field", "drf_yasg"]
+                    "django_countries", "phonenumber_field", "drf_yasg", "djcelery_email",]
 
 
 
@@ -147,6 +147,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_URL_REGEX = r"^/api/.*$"
 
 AUTH_USER_MODEL= "users.User"
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_RESULT_BACKEND_MAX_RETRIES = 10
+CELERY_TASK_SEND_SENT_EVENT = True
+
+if USE_TZ:
+    CELERY_TIMEZONE = TIME_ZONE
 
 LOGGING = {
     "version": 1,
