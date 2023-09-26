9f7bcbaa515cc93e7025ba449ce9aa97b62ba591
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-09-26 09:22:52 -0700

    feat: user can add,update and delete responses

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -55,7 +55,7 @@ THIRD_PARTY_APPS = [
 
 
 LOCAL_APPS=["core_apps.users", "core_apps.profiles", "core_apps.common","core_apps.articles",
-    "core_apps.ratings", "core_apps.bookmarks"]
+    "core_apps.ratings", "core_apps.bookmarks", "core_apps.responses"]
 
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
